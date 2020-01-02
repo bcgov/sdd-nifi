03b336d5a8f174362fe1884f6ba54b24ae46306f
Paul Ripley <Paul.Ripley@avocette.com>
2020-01-02 12:26:46 -0800

    capture multiple metadata validation errors (instead of just the first error)

--- nifi/scripts/foreign_key_consistency_check.py
+++ nifi/scripts/foreign_key_consistency_check.py
@@ -8,6 +8,18 @@ from datapackage import exceptions
 import zipfile
 import shutil
 
+err_str = ''
+def exc_handler(exc, row_number=None, row_data=None, error_data=None):
+    global err_str
+    if (type(exc).__name__ == 'UnresolvedFKError'):
+        errMessage = ''
+        if exc.multiple:
+            for error in exc.errors:
+                errMessage = errMessage + str(error) + ';'
+        else:
+            errMessage = str(exc)
+        err_str = err_str + 'Error:' + str(errMessage) + '\n' 
+
 try:
     path = sys.argv[2]
     # assumes that the datapackage 'resource' array has been added as flowfile attribute; assumes json has quotes replaced with ñÇ (to avoid nifi stripping quotes)
@@ -24,8 +36,12 @@ except:
 try:
     package = Package(dataPackageSchema, base_path=path)
     for resource in package.resources:
-        resource.check_relations()
-    print('success', end = '')
+        for r in resource.iter(integrity=False, relations=True, cast=True, foreign_keys_values=False, exc_handler=exc_handler):
+            continue
+    if (err_str == ''):
+        print('Success. Foreign key integrity check passed.', end = '')
+    else:
+        print(err_str, end = '')
     shutil.rmtree(path)
 except exceptions.RelationError as e:
     errMessage = ''
@@ -37,5 +53,5 @@ except exceptions.RelationError as e:
     print(errMessage, end = '')
     shutil.rmtree(path)
 except exceptions.DataPackageException: 
-    print('foreign key integrity check passed but other errors present.', end = '')
+    print('Foreign key integrity check passed but other errors present.', end = '')
     shutil.rmtree(path)
--- nifi/scripts/meta_consistency_check.py
+++ nifi/scripts/meta_consistency_check.py
@@ -5,7 +5,18 @@ import json
 from datapackage import Package
 from datapackage import exceptions
 
-# argv[1] is the data package schema
+err_str = ''
+def exc_handler(exc, row_number=None, row_data=None, error_data=None):
+    global err_str
+    errMessage = ''
+    if exc.multiple:
+        for error in exc.errors:
+            errMessage = errMessage + str(error) + ';'
+    else:
+        errMessage = str(exc)
+    err_str = err_str + 'line:' + str((row_number - 1)*fragmentIndex) + ' , error:' + str(errMessage) + '\n' 
+
+# argv[1] is the data package schema 
 revised = sys.argv[1].replace('ñÇ','"')
 try:
     dataPackageSchema=json.loads(revised)
@@ -15,11 +26,16 @@ except:
 
 # argv[2] is the resource in the data package to validate.  The 'data' elment for the resource should be populated (it's this data that is being validated)
 resourceName = sys.argv[2]
+fragmentIndex = int(sys.argv[3])
 try:
     package = Package(dataPackageSchema)
     resource = package.get_resource(resourceName)
-    resource.read(keyed=False, extended=False, cast=True, integrity=False, relations=False)
-    print('success', end = '')
+    for r in resource.iter(integrity=False, relations=False, cast=True, foreign_keys_values=False, exc_handler=exc_handler):
+        continue
+    if (err_str == ''):
+        print('success', end = '')
+    else:
+        print(err_str, end = '')
 except exceptions.DataPackageException as e:
     errMessage = ''
     if e.multiple:

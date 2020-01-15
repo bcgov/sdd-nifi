27539e5267f9d8d8a054a1d5db253550bd9eecbf
Paul Ripley <Paul.Ripley@avocette.com>
2020-01-15 14:57:32 -0800

    add data package generation

---
+++ nifi/scripts/generate_dp.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import sys
+import os
+from datapackage import Package
+from datapackage import exceptions
+import zipfile
+import shutil
+
+try:
+    path = sys.argv[1]
+    filename = sys.argv[2]
+    outputPath = sys.argv[3]
+    filePath = path + os.sep + filename
+    if filename.endswith('.zip'):
+        with zipfile.ZipFile(filePath,"r") as zip_ref:
+            zip_ref.extractall(path)
+    filenameNoExt = os.path.splitext(filename)[0]
+except:
+    shutil.rmtree(path)
+    print(sys.exc_info()[0])
+
+try:
+    package = Package()
+    #package.infer(path + os.sep + '*.csv')
+    package.infer('**/*.csv')
+    if not os.path.exists(outputPath):
+        os.mkdir(outputPath)
+    package.save(outputPath + os.sep + filenameNoExt + '_datapackage.zip')
+    print('Success. Data package created.', end = '')
+    #shutil.rmtree(path)
+except exceptions.DataPackageException as e:
+    errMessage = ''
+    if e.multiple:
+        for error in e.errors:
+            errMessage = errMessage + str(error) + ';'
+    else:
+        errMessage = str(e)
+    print(errMessage, end = '')
+    shutil.rmtree(path)
+except:
+    shutil.rmtree(path)
+    print(sys.exc_info()[0])

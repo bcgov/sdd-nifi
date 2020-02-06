14a359f33cac767f6741203a931cd65f5e672bfa
Paul Ripley <Paul.Ripley@avocette.com>
2020-02-06 15:13:43 -0800

    Add record processing flows and scripts

---
+++ nifi/scripts/get_column_names.sh
@@ -0,0 +1,20 @@
+#echo "Reading file=$1....."
+# print column names  from a CSV file (columms delimited by |)
+
+if [ $2 ]
+then
+    sep=$2
+else
+    sep=","
+fi
+
+
+cat $1 | head -1 | awk -F$sep '
+BEGIN{
+   counter=1
+}
+{ 
+  for(i=1; i <= NF; i++) 
+      print counter++ "). " $i
+      
+}'

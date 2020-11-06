d45eaeb747841c028c9fd7d3ef3bf8d3e78bf71b
Paul Ripley <Paul.Ripley@avocette.com>
2020-11-06 08:11:10 -0800

    refinements to manual approval scripts

--- nifi/scripts/approve_import.js
+++ nifi/scripts/approve_import.js
@@ -1,18 +1,27 @@
 const args = process.argv.slice(2)
 const file_to_approve = args[0];
-const approver = args[1];
-const approval_time = args[2];
+const current_dir = args[1];
+const approver = args[2];
+const approval_time = args[3];
 const approvaljson = {
   "approver": approver,
   "approval_time": approval_time
 }
-
-var file_json = require("./" + file_to_approve);
+full_path_to_file_to_approve = current_dir + '/' + file_to_approve
+var file_json = require(full_path_to_file_to_approve);
 file_json.rz_approval = approvaljson
-//console.log(JSON.stringify(file_json, null, 2));
-const approved_file_location = './approved/' + file_to_approve;
+
+const approved_file_location = current_dir + '/approved/' + file_to_approve;
 fs = require('fs');
 fs.writeFile(approved_file_location, JSON.stringify(file_json, null, 2), function (err) {
   if (err) return console.log(err);
   console.log(file_to_approve + ' has successfully been approved.');
 });
+//delete original file
+fs.unlink(full_path_to_file_to_approve, (err) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  //file removed
+})

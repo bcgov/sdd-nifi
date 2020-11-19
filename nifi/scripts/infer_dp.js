123ca43e79a2e909f9299b13db838241262a7826
Paul Ripley <Paul.Ripley@avocette.com>
2020-11-18 16:15:50 -0800

    add manual approval fields to source field

--- nifi/scripts/infer_dp.js
+++ nifi/scripts/infer_dp.js
@@ -2,10 +2,14 @@ const args = process.argv.slice(2)
 const data_file_name = args[0];
 const data_file_directory = args[1];
 process.env['NODE_CONFIG_DIR'] = args[2];
+const approver = args[3];
+const approval_time = args[4];
 const semanticinfer = require('semantic_infer');
 const source = {"sources": [{
   "title": data_file_name,
-  "path": data_file_directory
+  "path": data_file_directory,
+  "approver": approver,
+  "approval_time": approval_time
 }]}
 
 semanticinfer.datapackage_infer_filesystem.datapackage_infer_filesystem(source);

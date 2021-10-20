c32259492770945bc3e8096a8f9b89196e914a32
Paul Ripley <Paul.Ripley@avocette.com>
2021-10-20 15:44:39 -0700

    fix freq file generation issue with report generator

--- nifi/scripts/report_generator.sh
+++ nifi/scripts/report_generator.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 # Main validation script that runs on data that newly arrives in the red zone
-if [ $3 ]
-then
-	ext=$3
-else
-	ext=".csv"
-fi
 sep=","
 path=$1
+report_directory=$2
+mkdir -p $report_directory
 find $path -type f -name "* *" | while read file; do mv "$file" ${file// /_}; done
-# unzip the data file if needed
-if [ $2 == 'false' ]
-then
-	find $path -type f \( -name "*.zip" -or -name "*.ZIP" \) -print0 | sort |
-	while IFS= read -r -d '' file; do
-		unzip $file -d $path
-	done
-	find $path -type f \( -name "*.7z" -or -name "*.7Z" \) -print0 | sort |
-	while IFS= read -r -d '' file; do
-		7z x $file -o $path
-	done
-	echo ""
-fi
+
 find $path -type f \( -name "*.csv" -or -name "*.CSV" \) -print0 | sort |
 while IFS= read -r -d '' file; do
-	origfile=$(find $path -name $(basename $file .csv)${ext})
+	base_file_name=$(basename file)
 	dos2unix -q $file
 	#sanitize_csv $file
-	size=$(du -h $origfile | cut  -f1)B
+	size=$(du -h $file | cut  -f1)B
 	encoding=$(file -b --mime-encoding $file)
 	records=$(wc -l < $file)
 	# detect what separator (e.g., comma, pipe) is used to separate the column in the csv 
@@ -52,8 +36,8 @@ while IFS= read -r -d '' file; do
 	phnlines=$(./phn_finder.sh $file | wc -l)    
 	phonelines=$(./phone_finder.sh $file | wc -l)
 	# create a .tab file for each column in the csv.  Tab files have frequency of each value in the column. 
-	./chkfreq.sh $file >  "${file}.tab"
-	freqnm="${file}.tab"
+	./chkfreq.sh $file > "${report_directory}${base_file_name}.tab"
+	freqnm="${report_directory}${base_file_name}.tab"
 	# find any strings that could be common names e.g., Fred
 	namecnt=$(./scan_names.sh $freqnm)
 	# determine the md5 hash of the file

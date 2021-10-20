5500c58b46aa29c727098b12f04fda573a6a45cf
Paul Ripley <Paul.Ripley@avocette.com>
2021-10-19 17:11:38 -0700

    vastly descoped flow to support pivot to utilize PopExpress more

--- nifi/scripts/infer_dp.js
+++ nifi/scripts/infer_dp.js
@@ -1,21 +1,19 @@
 /*
 As part of the Nifi flow that processes of new data files in the secure area, we want to infer metadata (frictionless data package json)
-and as part of it add in who approved the data to be brought into the secure area and when.
+and as part of it add in additional information about the origin of the data.
 */
 const args = process.argv.slice(2)
 const data_file_name = args[0];
 const data_file_directory = args[1];
 process.env['NODE_CONFIG_DIR'] = args[2];
-const approver = args[3];
-const approval_time = args[4];
+const upload_info = args[3];
 const semanticinfer = require('semantic_infer');
 
 // add the "source" below to inferred frictionless data package json
 const source = {"sources": [{
   "title": data_file_name,
   "path": data_file_directory,
-  "approver": approver,
-  "approval_time": approval_time
+  "upload_info": upload_info
 }]}
 
 semanticinfer.datapackage_infer_filesystem.datapackage_infer_filesystem(source);

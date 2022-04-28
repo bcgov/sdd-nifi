2fafd4ce0cee806ce59f567fc378b31c5338c9a6
Paul Ripley <Paul.Ripley@avocette.com>
2022-04-28 08:14:22 -0700

    improved common name search criteria

--- nifi/scripts/scan_names.sh
+++ nifi/scripts/scan_names.sh
@@ -4,7 +4,7 @@ freqFile=$1
 total=0
 input="listofnames.txt"
 while IFS= read -r line; do
-	nameCt=$(grep -i "$line" $freqFile | cut -d" " -f2)
+	nameCt=$(grep -i "^$line " $freqFile | cut -d" " -f2)
 	if [ $nameCt > 0 ]
 	then
 		total=$(($total+$nameCt))
